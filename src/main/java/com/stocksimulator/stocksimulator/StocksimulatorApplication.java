23fbac90246d2c565ba04fc43c5b3d9a84f1b8a0
dewanshpratapsingh <dewansh.mca21.du@gmail.com>
2025-08-05 08:33:30 +0000

    Initiated folder structure

---
+++ src/main/java/com/stocksimulator/stocksimulator/StocksimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.stocksimulator.stocksimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocksimulator/stocksimulator/StocksimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksimulator.stocksimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
